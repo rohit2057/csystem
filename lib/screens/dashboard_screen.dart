ff04c92076900142e0516120e598c92486b01247
rohit2057 <roheetraut2057@gmail.com>
2024-05-20 22:49:51 +0545

    home

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -10,8 +10,33 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Dashboard")),
+    return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      home: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 1, 134, 243),
+          iconTheme: const IconThemeData(
+            color: Colors.white, // Change the drawer icon color to white
+          ),
+          actions: const [
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              Icons.supervised_user_circle_outlined,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              Icons.mode_of_travel,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
