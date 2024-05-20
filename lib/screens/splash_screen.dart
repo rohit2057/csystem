098d7ad0e7910b8a275b9b13d94ada8271ad6dc9
rohit2057 <roheetraut2057@gmail.com>
2024-05-20 20:51:46 +0545

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:csystem/screens/dashboard_screen.dart';
+import 'package:csystem/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      openLogin();
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void openDashboard() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return DashboardScreen();
+    }));
+  }
+
+  void openLogin() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LoginScreen();
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Image.asset("assets/images/ngov.png"),
+        ),
+      ),
+    );
+  }
+}
